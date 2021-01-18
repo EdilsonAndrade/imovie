fdacb39c4621ac75c2874dc38875229f2d54bd95
Edilson Andrade <edilsonandrade@ext.submarinoviagens.com.br>
2021-01-18 11:57:09 -0300

    fixing the query regarding stars

--- src/hooks/movie.tsx
+++ src/hooks/movie.tsx
@@ -4,42 +4,42 @@ import React, {
 import PropTypes from 'prop-types';
 import api from '../services/api';
 
-export interface IMovieData{
-    id:number;
-    backdrop_path:string;
-    overview:string;
-    poster_path:string;
-    title:string;
-    vote_average:number;
-    vote_count:number;
-    adult:boolean;
-    showDetail:boolean;
-    release_date:string;
+export interface IMovieData {
+  id: number;
+  backdrop_path: string;
+  overview: string;
+  poster_path: string;
+  title: string;
+  vote_average: number;
+  vote_count: number;
+  adult: boolean;
+  showDetail: boolean;
+  release_date: string;
 }
 
-interface AuthMovieData{
-  movie?:IMovieData[];
-  getMovies():Promise<void>;
-  searchMovies(title:string):void;
-  filterByRate(starId:number):void;
+interface AuthMovieData {
+  movie?: IMovieData[];
+  getMovies(): Promise<void>;
+  searchMovies(title: string): void;
+  filterByRate(starId: number): void;
 
 }
 
 const MovieContext = createContext<AuthMovieData>({} as AuthMovieData);
 
-const MovieProvider:React.FC = ({ children }) => {
+const MovieProvider: React.FC = ({ children }) => {
   const [movieData, setMovieData] = useState<IMovieData[]>();
   const [originalMovieData, setOriginalMovieData] = useState<IMovieData[]>();
   const getMovies = useCallback(async () => {
     const response = await api.get(`/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&sort_by=popularity.desc`);
 
-    const movies:IMovieData[] = [];
+    const movies: IMovieData[] = [];
     const { results } = response.data;
     const options = {
       month: 'long', day: 'numeric',
     };
     if (results) {
-      results.forEach((m:IMovieData) => {
+      results.forEach((m: IMovieData) => {
         movies.push({
           ...m,
           release_date: new Intl.DateTimeFormat('en-US', options).format(new Date(m.release_date)),
@@ -50,7 +50,7 @@ const MovieProvider:React.FC = ({ children }) => {
     setOriginalMovieData(movies);
   }, []);
 
-  const searchMovies = useCallback((title:string) => {
+  const searchMovies = useCallback((title: string) => {
     if (title.length > 0) {
       const movieFiltered = originalMovieData?.filter((m) => m.title.toLowerCase()
         .includes(title.toLowerCase()));
@@ -61,13 +61,13 @@ const MovieProvider:React.FC = ({ children }) => {
     }
   }, [originalMovieData]);
 
-  const filterByRate = useCallback((starId:number) => {
+  const filterByRate = useCallback((starId: number) => {
     const movieRates = [2, 4, 6, 8, 10];
 
     if (starId > 0) {
       const movieFiltered = originalMovieData?.filter(
-        (m) => (Number(m.vote_average.toFixed()) >= movieRates[starId - 1] - 2
-           && Number(m.vote_average.toFixed()) <= movieRates[starId - 1]),
+        (m) => m.vote_average >= (movieRates[starId - 1] - 2)
+         && m.vote_average <= movieRates[starId - 1],
       );
 
       setMovieData(movieFiltered);
@@ -85,7 +85,7 @@ const MovieProvider:React.FC = ({ children }) => {
   );
 };
 
-function useMovie():AuthMovieData {
+function useMovie(): AuthMovieData {
   const context = useContext(MovieContext);
   if (!context) {
     throw new Error('useMovie must be used within an MovieProvider');
